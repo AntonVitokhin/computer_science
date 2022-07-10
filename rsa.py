b4203ae28ad714b34dc339d35f139cd856d27ad9
Anton Vitokhin <tosha.vitohin@mail.ru>
2022-07-10 23:02:47 +1000

    Добавлена функция проверяющая число Эйлера

--- rsa.py
+++ rsa.py
@@ -9,3 +9,11 @@ def gcd(num1, num2):  # НОД.
             num2 %= num1
 
     return num1 + num2
+
+
+def check_e(num):  # Проверка числа е. Простое, меньше функции Эйлера, взаимно простое с функцией Эйлера.
+    condition1 = is_prime(num)
+    condition2 = num < eulerFun
+    condition3 = gcd(num, eulerFun) == 1
+
+    return condition1 and condition2 and condition3
