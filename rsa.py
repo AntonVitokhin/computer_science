df86cba3d962c2b63e97b01856e48358899ceaf9
Anton Vitokhin <tosha.vitohin@mail.ru>
2022-07-10 23:04:47 +1000

    Добавлена функция проверяющая число на простоту

---
+++ rsa.py
@@ -0,0 +1,11 @@
+def is_prime(num):  # Проверка на простое число.
+    if num == 2 or num == 3:
+        return True
+    if num % 2 == 0 or num < 2:
+        return False
+
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num % i == 0:
+            return False
+
+    return True
